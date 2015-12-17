ba7b6713e3bccdc3fa04937af97345a06dd08326
Alexandre Boyer <biggnou@gmail.com>
2015-12-16 20:58:16 -0500

    flag counter works

--- getflags.py
+++ getflags.py
@@ -1,12 +1,13 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 
-import sys
 import argparse
-import scapy.all as scapy
+import scapy.all as s
 
 def dostuff( pcap ):
-    pkts = scapy.rdpcap(pcap)
+    totpkt = totsyn = totsynack = totack = totfin = 0
+
+    pkts = s.rdpcap(pcap)
     flags = {
     	'F': 'FIN',
 	'S': 'SYN',
@@ -17,10 +18,22 @@ def dostuff( pcap ):
     	'E': 'ECE',
     	'C': 'CWR',
 	}
-    for p in pkts:
-    	[flags[x] for x in p.sprintf('%TCP.flags%')]
 
-pcap = ''
+    for p in pkts:
+        totpkt += 1
+        if 'SYN' in [flags[x] for x in p.sprintf('%TCP.flags%')]:
+            totsyn += 1
+            if 'ACK' in [flags[x] for x in p.sprintf('%TCP.flags%')]:
+                totsynack += 1
+        if 'FIN' in [flags[x] for x in p.sprintf('%TCP.flags%')]:
+            totfin += 1
+        if 'ACK' in [flags[x] for x in p.sprintf('%TCP.flags%')]:
+            totack += 1
+            # if 'SYN' in [flags[x] for x in p.sprintf('%TCP.flags%')]:
+            #     totack -= 1
+        print [flags[x] for x in p.sprintf('%TCP.flags%')]
+
+    print('Total SYN:', totsyn, 'total SYN-ACK:', totsynack , 'and total ACK:', totack, 'total FIN:', totfin, 'for total pkts:', totpkt)
 
 def main():
     parser = argparse.ArgumentParser()
